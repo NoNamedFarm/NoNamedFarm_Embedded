637d87c2fa878f866fa989afc55504ab8a00d025
choiyj0812 <cyjin1513@gmail.com>
2022-10-27 23:50:42 +0900

    AF
    add function about mp3 player module

--- Final/Final.ino
+++ Final/Final.ino
@@ -6,24 +6,24 @@
 #include <LiquidCrystal_I2C.h>
 #include <HardwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
-
+//========================================
 HardwareSerial dfp(2);
 DFRobotDFPlayerMini mp3;
 //임시 정의
 #define DF_tx 22
 #define DF_rx 23
-
+//----------------------------------------
 #define DHTPIN 33
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
-
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
-//LiquidCrystal_I2C lcd(0x27, 16, 2);
-
+//----------------------------------------
 #define SOIL 14
-
+//----------------------------------------
 #define Motor_Relay 32
 #define LED_Relay 25
+//----------------------------------------
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+//LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 byte happy[6][8] = {
   {0b00000, 0b00011, 0b00100, 0b01000, 0b01000, 0b01000, 0b10000, 0b10000},
@@ -42,24 +42,23 @@ byte tired[6][8] = {
   {0b00000, 0b00000, 0b11111, 0b00000, 0b00000, 0b00000, 0b00000, 0b11111},
   {0b00001, 0b00001, 0b00010, 0b10010, 0b00010, 0b00100, 0b11000, 0b00000}
 };
-
-IPAddress server_addr(192,168,137,186);  // IP of the MySQL *server* here
-char user[] = "root";            // MySQL user login username
-char password[] = "1234";         // MySQL user login password
-
-// WiFi card example
-const char ssid[] = "CDMA2000";          // your SSID
-const char pass[] = "1q2w3e4r";      // your SSID Password
-
-WiFiClient client;            // Use this for WiFi instead of EthernetClient
+//----------------------------------------
+IPAddress server_addr(192,168,137,186);
+char user[] = "root";
+char password[] = "1234";
+//----------------------------------------
+const char ssid[] = "CDMA2000";
+const char pass[] = "1q2w3e4r";
+//----------------------------------------
+WiFiClient client;
 MySQL_Connection conn((Client *)&client);
 MySQL_Cursor cur = MySQL_Cursor(&conn);
-
+//----------------------------------------
 #define Move_Insert 5000
 #define Move_Select 5000
 #define Move_Sensor 500
 #define Move_Actuator 500
-
+//========================================
 void setup_Face(bool face){
   if(face == true){
     for(int i=0; i<6; i++){
@@ -72,14 +71,14 @@ void setup_Face(bool face){
     }
   }
 }
-
+//----------------------------------------
 void print_Face(){
   lcd.setCursor(6,0);
   for(int i=0; i<3; i++) lcd.write(0x00 + i);
   lcd.setCursor(6,1);
   for(int i=0; i<3; i++) lcd.write(0x03 + i);
 }
-
+//----------------------------------------
 void WiFi_init(){
   Serial.println();
   Serial.println();
@@ -98,7 +97,7 @@ void WiFi_init(){
   String ip = WiFi.localIP().toString();
   Serial.printf("[WiFi] Connected %s\n", ip.c_str());
 }
-
+//----------------------------------------
 void DB_init(){
   Serial.println("[DB] Connecting...");
   if (conn.connect(server_addr, 3306, user, password)) {
@@ -107,12 +106,12 @@ void DB_init(){
   }
   else Serial.println("[DB] Connection failed.");
 }
-
+//----------------------------------------
 void mp3_init(){
   dfp.begin(9600, SERIAL_8N1, DF_tx, DF_rx);
   mp3.begin(dfp);
 }
-
+//========================================
 void setup() {
   dht.begin();
   lcd.init();
@@ -132,7 +131,7 @@ void setup() {
   DB_init();
   mp3_init();
 }
-
+//========================================
 float Temp = 0;
 int Soil = 0;
 bool Check_Water = false;
@@ -145,10 +144,11 @@ unsigned long actuator_time = 0;
 
 char INSERT_SQL[] = "INSERT INTO test_arduino.hello_arduino (message) VALUES ('Hello, Arduino!')";
 char SELECT_SQL[] = "SELECT Light FROM test.test";
-
+long long Device = 3;
+//========================================
 void loop() {
   unsigned long Now = millis();
-
+  //----------------------------------------
   if(Now - sensor_time > Move_Sensor){
     sensor_time = Now;
 
@@ -171,7 +171,7 @@ void loop() {
       print_Face();
     }
   }
-
+  //----------------------------------------
   if(Now - actuator_time > Move_Actuator){
     actuator_time = Now;
     
@@ -189,18 +189,18 @@ void loop() {
       digitalWrite(Motor_Relay, LOW);
     }
   }
-  
+  //----------------------------------------
   if(Now - Insert_time > Move_Insert){
     Insert_time = Now;
 
     //Insert temp&humi
-    sprintf(INSERT_SQL, "INSERT INTO project.data(Temp, Humi) Values (%.2f, %d)", Temp, Soil);
+    sprintf(INSERT_SQL, "INSERT INTO project.data(id, Temp, Humi) Values (%lld, %.2f, %d)", Device, Temp, Soil);
   
     if(conn.connected()){
       cur.execute(INSERT_SQL);
     }
   }
-
+  //----------------------------------------
   if(Now - Select_time > Move_Select){
     Select_time = Now;
     
