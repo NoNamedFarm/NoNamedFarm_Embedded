661acd1c90164f0062b18eee2db7058373a033bc
choiyj0812 <cyjin1513@gmail.com>
2022-10-27 13:15:43 +0900

    Fix
    Success to DB

--- Final/Final.ino
+++ Final/Final.ino
@@ -5,21 +5,17 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-#define DHTPIN 0
+#define DHTPIN 33
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-#define SCL 5
-#define SDA 4
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 //LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-#define SOIL A0
+#define SOIL 14
 
-#define M1 5
-#define M2 4
-
-#define Relay 16
+#define Motor_Relay 32
+#define LED_Relay 25
 
 byte happy[6][8] = {
   {0b00000, 0b00011, 0b00100, 0b01000, 0b01000, 0b01000, 0b10000, 0b10000},
@@ -39,19 +35,22 @@ byte tired[6][8] = {
   {0b00001, 0b00001, 0b00010, 0b10010, 0b00010, 0b00100, 0b11000, 0b00000}
 };
 
-byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-
-//IPAddress server_addr(10,0,1,35);  // IP of the MySQL *server* here
-IPAddress server_addr("domain : diary-database.ctmdbcffdqs4.ap-northeast-2.rds.amazonaws.com/heal");
-char user[] = "root";              // MySQL user login username
-char password[] = "mexfav-9tAjjo-hypveh";        // MySQL user login password
+IPAddress server_addr(192,168,137,186);  // IP of the MySQL *server* here
+char user[] = "root";            // MySQL user login username
+char password[] = "1234";         // MySQL user login password
 
 // WiFi card example
-char ssid[] = "2316";            // your SSID
-char pass[] = "999999999";       // your SSID Password
+const char ssid[] = "CDMA2000";          // your SSID
+const char pass[] = "1q2w3e4r";      // your SSID Password
 
 WiFiClient client;            // Use this for WiFi instead of EthernetClient
 MySQL_Connection conn((Client *)&client);
+MySQL_Cursor cur = MySQL_Cursor(&conn);
+
+#define Move_Insert 5000
+#define Move_Select 5000
+#define Move_Sensor 500
+#define Move_Actuator 500
 
 void setup_Face(bool face){
   if(face == true){
@@ -78,38 +77,41 @@ void setup() {
   lcd.init();
   lcd.backlight();
 
-  pinMode(M1, OUTPUT);
-  pinMode(M2, OUTPUT);
-  pinMode(Relay, OUTPUT);
-  digitalWrite(M1, LOW);
-  digitalWrite(M2, LOW);
-  digitalWrite(Relay, LOW);
+  pinMode(Motor_Relay, OUTPUT);
+  pinMode(LED_Relay, OUTPUT);
+  digitalWrite(Motor_Relay, LOW);
+  digitalWrite(LED_Relay, LOW);
+
+  delay(10);
   
   Serial.begin(115200);
   while (!Serial); // wait for serial port to connect. Needed for Leonardo only
 
+  Serial.println();
+  Serial.println();
+  Serial.print("[WiFi] Connecting to ");
+  Serial.println(ssid);
+
   // Begin WiFi section
-  int status = WiFi.begin(ssid, pass);
-  if ( status != WL_CONNECTED) {
-    Serial.println("Couldn't get a wifi connection");
-    while(true);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(100);
   }
-  // print out info about the connection:
-  else {
-    Serial.println("Connected to network");
-    IPAddress ip = WiFi.localIP();
-    Serial.print("My IP address is: ");
-    Serial.println(ip);
-  }
+  Serial.println();
+
+  String ip = WiFi.localIP().toString();
+  Serial.printf("[WiFi] Connected %s\n", ip.c_str());
   // End WiFi section
 
-  Serial.println("Connecting...");
+  Serial.println("[DB] Connecting...");
   if (conn.connect(server_addr, 3306, user, password)) {
     delay(1000);
+    Serial.println("[DB] Success to DB!");
   }
-  else
-    Serial.println("Connection failed.");
-  conn.close();
+  else Serial.println("[DB] Connection failed.");
 }
 
 float Temp = 0;
@@ -120,6 +122,7 @@ bool Check_Light = false;
 unsigned long Insert_time = 0;
 unsigned long Select_time = 0;
 unsigned long sensor_time = 0;
+unsigned long actuator_time = 0;
 
 char INSERT_SQL[] = "INSERT INTO test_arduino.hello_arduino (message) VALUES ('Hello, Arduino!')";
 char SELECT_SQL[] = "SELECT Light FROM test.test";
@@ -127,19 +130,17 @@ char SELECT_SQL[] = "SELECT Light FROM test.test";
 void loop() {
   unsigned long Now = millis();
 
-  if(Now - sensor_time > 30000){
+  if(Now - sensor_time > Move_Sensor){
     sensor_time = Now;
 
     Temp = dht.readTemperature();
     Soil = analogRead(SOIL);
 
     if(isnan(Temp)){
-      Serial.println("Failed to read from DHT11!");
-      continue;
+      Serial.println("[DHT11] Failed to read");
     }
     if(isnan(Soil)){
-      Serial.println("Failed to read from Soil_Sensor");
-      continue;
+      Serial.println("[Soil_Sensor] Failed to read");
     }
 
     if((Temp < 30 && Temp > 10) && (Soil < 768 && Soil > 256)){
@@ -151,91 +152,73 @@ void loop() {
       print_Face();
     }
   }
-  
-  if(Now - Insert_time > 30000){
-    Insert_time = Now;
+
+  if(Now - actuator_time > Move_Actuator){
+    actuator_time = Now;
     
-    if(WiFi.status != WL_CONNECTED){
-      Serial.println("try reconnect");
-  
-      WiFi.begin(ssid, pass);
-      while(WiFi.status() != WL_CONNECTED){
-        delay(500);
-        Serial.print(".");
-      }
-  
-      Serial.println();
-      Serial.println("WiFi Connected!");
-      Serial.println();
-      Serial.println("Wifi connected!");
-      Serial.println("\nConnected to network");
-      Serial.print("My IP address is: ");
-      Serial.println(WiFi.localIP());
+    if(Check_Light){
+      digitalWrite(LED_Relay, HIGH);
     }
+    else{
+      digitalWrite(LED_Relay, LOW);
+    }
+
+    if(Check_Water){
+      digitalWrite(Motor_Relay, HIGH);
+    }
+    else{
+      digitalWrite(Motor_Relay, LOW);
+    }
+  }
   
-    sprintf(INSERT_SQL, "INSERT INTO test.test Values (%d, %d)", Temp, Soil);
+  if(Now - Insert_time > Move_Insert){
+    Insert_time = Now;
+
+    //Insert temp&humi
+    sprintf(INSERT_SQL, "INSERT INTO project.data(Temp, Humi) Values (%.2f, %d)", Temp, Soil);
   
     if(conn.connected()){
-      cursor.execute(INSERT_SQL);
+      cur.execute(INSERT_SQL);
     }
   }
 
-  if(Now - Select_time > 30000){
-    Select_time - Now;
-
-    if(WiFi.status != WL_CONNECTED){
-      Serial.println("try reconnect");
-  
-      WiFi.begin(ssid, pass);
-      while(WiFi.status() != WL_CONNECTED){
-        delay(500);
-        Serial.print(".");
-      }
-  
-      Serial.println();
-      Serial.println("WiFi Connected!");
-      Serial.println();
-      Serial.println("Wifi connected!");
-      Serial.println("\nConnected to network");
-      Serial.print("My IP address is: ");
-      Serial.println(WiFi.localIP());
-    }
+  if(Now - Select_time > Move_Select){
+    Select_time = Now;
     
     row_values *row = NULL;
-    long head_count = 0;
-    delay(1000);
+    String Cmd = "";
+    //--------------------------------------------------------
+    //Select Water
+    sprintf(SELECT_SQL, "SELECT Water FROM project.cmd");
+    if(conn.connected()){
+      cur.execute(SELECT_SQL);
+      cur.get_columns();
+      do{
+        row = cur.get_next_row();
+        if(row != NULL){
+          Cmd = row->values[0];
+        }
+      } while(row != NULL);
+      cur.close();
+    }
+    if(Cmd == "1") Check_Water = true;
+    else if(Cmd == "0") Check_Water = false;
 
-    MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-    SELECT_SQL[] = "SELECT Light FROM test.test";
-    cur_mem.execute(Select_SQL);
-    column_names *columns = cur_mem.get_columns();
-
-    do{
-      row = cur_mem.get_next_row();
-      if(row != NULL){
-        head_count = atoi(row.values[0]);
-      }
-    } while(row != NULL);
-    delete cur_mem;
-
-    Serial.print("NYC pop = ");
-    Serial.println(head_count);
-    delay(500);
-
-    SELECT_SQL[] = "SELECT Water FROM test.test";
-    cur.execute(Select_SQL);
-    cur.get_columns();
-    do{
-      row = cur.get_next_row();
-      if(row != NULL){
-        head_count = atoi(row.values[0]);
-      }
-    } while(row != NULL);
-    cur.close();
-
-    Serial.print("NYC pop = ");
-    Serial.pritnln(head_count);
-    Serial.print("NYC pop increased by 12 = ");
-    Serial.println(head_count + 12);
+    //--------------------------------------------------------
+    //Select Light
+    sprintf(SELECT_SQL, "SELECT Light FROM project.cmd");
+    if(conn.connected()){
+      cur.execute(SELECT_SQL);
+      cur.get_columns();
+      do{
+        row = cur.get_next_row();
+        if(row != NULL){
+          Cmd = row->values[0];
+        }
+      } while(row != NULL);
+      cur.close();
+    }
+    if(Cmd == "1") Check_Light = true;
+    else if(Cmd == "0") Check_Light = false;
   }
 }
